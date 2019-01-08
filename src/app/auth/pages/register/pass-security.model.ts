df0de77c7b032d4e8602e56ddbd58776ef6854b5
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-08 00:15:08 -0200

    Implementando validações e integração com reactive forms do angular

---
+++ src/app/auth/pages/register/pass-security.model.ts
@@ -0,0 +1,8 @@
+enum PassSecurity {
+  WEAK = 0,
+  MEDIUM = 1,
+  STRONG = 2,
+  VERY_STRONG = 3
+}
+
+export default PassSecurity;
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,8 +1,69 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import PassSecurity from './pass-security.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent { }
+export class RegisterComponent implements AfterViewInit {
+
+  @ViewChild('firstInput') firstInput: ElementRef<HTMLInputElement>;
+
+  form: FormGroup;
+
+  name = new FormControl('', [ Validators.required ]);
+  email = new FormControl('', [ Validators.required, Validators.email ]);
+  password = new FormControl('', [ Validators.required, RegisterComponent.strongPassword ]);
+  passwordAgain = new FormControl('', [ Validators.required, RegisterComponent.sameValue(this.password) ]);
+  admin = new FormControl(false);
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      passwordAgain: this.passwordAgain,
+      admin: this.admin
+    });
+  }
+
+  static sameValue(otherControl: AbstractControl): ValidatorFn {
+    return (control: AbstractControl) => {
+      return !control.value || otherControl.value === control.value ? null : { sameValue: true };
+    };
+  }
+
+  static checkPasswordSecurity(password: string): PassSecurity {
+    let security = 0;
+
+    if (/[0-9]+/.test(password) && /[A-Z]+/.test(password) && /[a-z]+/.test(password)) {
+      security++;
+    }
+
+    if (/[#@%^&*()_+!~?]+/.test(password)) {
+      security++;
+    }
+
+    if (password.length >= 6) {
+      security++;
+    } else {
+      security = Math.max(0, security - 1);
+    }
+
+    return security;
+  }
+
+  static strongPassword(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) {
+      return null;
+    }
+    return RegisterComponent.checkPasswordSecurity(control.value) >= PassSecurity.STRONG ? null : { strongPassword: true };
+  }
+
+  ngAfterViewInit() {
+    this.firstInput.nativeElement.focus();
+  }
+
+}
