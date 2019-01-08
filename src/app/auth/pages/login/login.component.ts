2836c7f7721cd6aee96e77c36ab00d1815366d35
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-08 16:41:37 -0200

    Adicionando outro exemplo, agora obtendo os parametros de um observable

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   identification: string;
+  asyncIdentification: string;
+
+  subscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,6 +21,16 @@ export class LoginComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
 
     this.identification = id;
+
+    this.subscription = this.route.paramMap.pipe(
+      map(param => param.get('id'))
+    ).subscribe(paramId => {
+      this.asyncIdentification = paramId;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
