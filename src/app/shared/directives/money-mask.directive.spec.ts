6fa59d830e32ce2ba7c55852930ca815ed680fe5
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-11 15:57:25 -0200

    Criando teste para diretiva

--- src/app/shared/directives/money-mask.directive.spec.ts
+++ src/app/shared/directives/money-mask.directive.spec.ts
@@ -1,10 +1,55 @@
 import { MoneyMaskDirective } from './money-mask.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  template: `
+    <form [formGroup]="form">
+      <input type="text" formControlName="value" appMoneyMask>
+    </form>
+  `
+})
+class TestComponent {
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      value: ['']
+    });
+  }
+}
 
 describe('MoneyMaskDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new MoneyMaskDirective();
-  //   expect(directive).toBeTruthy();
-  // });
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
 
-  it('TODO');
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      declarations: [ MoneyMaskDirective, TestComponent ]
+    }).createComponent(TestComponent);
+
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should format currency number', () => {
+    component.form.setValue({
+      value: '12000.50'
+    });
+
+    const input = fixture.nativeElement.querySelector('input');
+
+    expect(input.value).toBe('12.000,50');
+  });
 });
