d795f69fa3a58a54d39352070d0461c0f833f65e
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-11 10:02:09 -0200

    Criando teste de componente com @Input, @Output

--- src/app/dashboard/components/header/header.component.spec.ts
+++ src/app/dashboard/components/header/header.component.spec.ts
@@ -16,12 +16,33 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.userPhoto = 'https://angular.io/assets/images/logos/angular/angular.svg';
+    component.userName = 'First Second Name';
+
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  it('TODO');
+  it('should display user photo and name values', () => {
+    const photo: HTMLImageElement = fixture.nativeElement.querySelector('img');
+    const banner = fixture.nativeElement.querySelector('.banner h1').textContent;
+    const profile = fixture.nativeElement.querySelector('.profile span').textContent;
+
+    expect(photo.src).toBe('https://angular.io/assets/images/logos/angular/angular.svg');
+
+    expect(banner).toBe('Olá First');
+    expect(profile).toBe('First Second Name');
+  });
+
+  it('should emit logout event when click exit button', () => {
+    spyOn(component.logout, 'emit');
+
+    const logoutButton = fixture.nativeElement.querySelector('.exit');
+    logoutButton.click();
+
+    expect(component.logout.emit).toHaveBeenCalled();
+  });
 });
