36dd1c64a848169783f6efba6ac0bf4ff8613788
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-11 12:14:49 -0200

    Criando teste para service com requests http

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -1,14 +1,68 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { BASE_API_URL } from '../../../tokens';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AuthService;
+  let http: HttpTestingController;
 
-  // it('should be created', () => {
-  //   const service: AuthService = TestBed.get(AuthService);
-  //   expect(service).toBeTruthy();
-  // });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: BASE_API_URL, useValue: 'http://api.com' }
+      ],
+      imports: [ HttpClientTestingModule ]
+    });
 
-  it('TODO');
+    service = TestBed.get(AuthService);
+    http = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
+  });
+
+  it('should created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should make a http request on login', () => {
+    const data = {
+      email: 'admin@admin.com',
+      password: 'admin'
+    };
+
+    service.login(data.email, data.password).subscribe();
+
+    const req = http.expectOne('http://api.com/v1/auth/login');
+
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(data);
+  });
+
+  it('should make a http request on register', () => {
+    const data = {
+      name: 'User Name',
+      email: 'email@email.com',
+      password: 'pass',
+      admin: true
+    };
+
+    service.register(data).subscribe();
+
+    const req = http.expectOne('http://api.com/v1/auth/register');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(data);
+  });
+
+  it('should make a http request on checkUniqueEmail', () => {
+    const email = 'admin@admin.com';
+
+    service.checkUniqueEmail(email).subscribe();
+
+    const req = http.expectOne(`http://api.com/v1/auth/unique-email?email=${email}`);
+    expect(req.request.method).toBe('GET');
+  });
 });
--- src/app/dashboard/services/purchases.service.spec.ts
+++ src/app/dashboard/services/purchases.service.spec.ts
@@ -1,14 +1,80 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PurchasesService } from './purchases.service';
+import Purchase from '../models/purchase.model';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { BASE_API_URL } from '../../../tokens';
 
 describe('PurchasesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  const purchase: Purchase = {
+    id: '1',
+    description: 'Description',
+    quantity: 10,
+    date: '2019-01-10',
+    paid: true,
+    value: 1,
+    total: 10
+  };
 
-  // it('should be created', () => {
-  //   const service: PurchasesService = TestBed.get(PurchasesService);
-  //   expect(service).toBeTruthy();
-  // });
+  let service: PurchasesService;
+  let http: HttpTestingController;
 
-  it('TODO');
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: BASE_API_URL, useValue: 'http://api.com' }
+      ],
+      imports: [ HttpClientTestingModule ]
+    });
+
+    service = TestBed.get(PurchasesService);
+    http = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should make a http request on search', () => {
+    const description = 'description';
+
+    service.search(description).subscribe();
+
+    const req = http.expectOne(`http://api.com/v1/purchases?search=1&description=${description}`);
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should make a http request on all', () => {
+    service.all().subscribe();
+
+    const req = http.expectOne('http://api.com/v1/purchases');
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should make a http request on create', () => {
+    service.create(purchase).subscribe();
+
+    const req = http.expectOne('http://api.com/v1/purchases');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(purchase);
+  });
+
+  it('should make a http request on update', () => {
+    service.update(purchase).subscribe();
+
+    const req = http.expectOne(`http://api.com/v1/purchases/${purchase.id}`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(purchase);
+  });
+
+  it('should make a http request on delete', () => {
+    service.delete(purchase.id).subscribe();
+
+    const req = http.expectOne(`http://api.com/v1/purchases/${purchase.id}`);
+    expect(req.request.method).toBe('DELETE');
+  });
 });
