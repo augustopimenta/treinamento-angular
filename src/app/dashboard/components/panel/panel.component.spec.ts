5bfdc1d31c2b3b0e44ae63bf3834c266062949c2
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-11 10:58:08 -0200

    Criando teste de componente com projection

--- src/app/dashboard/components/panel/panel.component.spec.ts
+++ src/app/dashboard/components/panel/panel.component.spec.ts
@@ -1,27 +1,62 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PanelComponent } from './panel.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <app-panel>
+      <button header>Test</button>
+      <span>Content</span>
+    </app-panel>
+  `
+})
+class TestComponent {}
 
 describe('PanelComponent', () => {
   let component: PanelComponent;
   let fixture: ComponentFixture<PanelComponent>;
+  let testFixture: ComponentFixture<TestComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PanelComponent ]
+      declarations: [ PanelComponent, TestComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PanelComponent);
+    testFixture = TestBed.createComponent(TestComponent);
+
     component = fixture.componentInstance;
+    component.header = 'Title';
+    component.description = 'Description';
+
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  it('TODO');
+  it('should display title and description value', () => {
+    const title = fixture.nativeElement.querySelector('.left h1').textContent;
+    const description = fixture.nativeElement.querySelector('.left p').textContent;
+
+    expect(title).toBe('Title');
+    expect(description).toBe('Description');
+  });
+
+  it('should display inner content with projection', () => {
+    const button = testFixture.nativeElement.querySelector('.right button');
+    const content = testFixture.nativeElement.querySelector('.content span');
+
+    expect(button).toBeTruthy();
+    expect(button.textContent).toBe('Test');
+
+    expect(content).toBeTruthy();
+    expect(content.textContent).toBe('Content');
+  });
 });
