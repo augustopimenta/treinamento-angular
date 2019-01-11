c16653f94c877925a984b776a06cf324eddb68ac
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-11 13:33:13 -0200

    Criando teste para componente com subject

--- src/app/core/components/alert/alert.component.spec.ts
+++ src/app/core/components/alert/alert.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
+import Alert from '../../models/alert.model';
+import { BehaviorSubject } from 'rxjs';
+import AlertType from '../../enums/alert-type.enum';
+import { AlertService } from '../../services/alert.service';
 
 describe('AlertComponent', () => {
   let component: AlertComponent;
   let fixture: ComponentFixture<AlertComponent>;
 
+  const alertServiceMock = {
+    data$: new BehaviorSubject<Alert>({
+      open: false,
+      type: AlertType.SUCCESS,
+      message: ''
+    }),
+    hide: jasmine.createSpy('hide')
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlertComponent ]
+      declarations: [ AlertComponent ],
+      providers: [
+        { provide: AlertService, useValue: alertServiceMock }
+      ]
     })
     .compileComponents();
   }));
@@ -19,9 +35,30 @@ describe('AlertComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  it('TODO');
+  it('should display alert when receive data from observable', () => {
+    alertServiceMock.data$.next({
+      open: true,
+      type: AlertType.SUCCESS,
+      message: 'Message'
+    });
+
+    fixture.detectChanges();
+
+    const alert = fixture.nativeElement.querySelector('.alert');
+
+    expect(alert.classList.contains('show')).toBe(true);
+    expect(alert.classList.contains('alert-success')).toBe(true);
+    expect(alert.textContent).toContain('Message');
+  });
+
+  it('should call service hide on click close button', () => {
+    const button = fixture.nativeElement.querySelector('.close');
+    button.click();
+
+    expect(alertServiceMock.hide).toHaveBeenCalled();
+  });
 });
--- src/app/core/components/dialog/dialog.component.spec.ts
+++ src/app/core/components/dialog/dialog.component.spec.ts
@@ -1,14 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { DialogComponent } from './dialog.component';
+import { DialogService } from '../../services/dialog.service';
+import Dialog from '../../models/dialog.model';
+import { Subject } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ModalComponent } from '../../../shared/components/modal/modal.component';
+import { CloseModalDirective } from '../../../shared/components/modal/close-modal.directive';
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
 
+  const dialogServiceMock = {
+    data$: new Subject<Dialog>()
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DialogComponent ]
+      declarations: [ DialogComponent, ModalComponent, CloseModalDirective ],
+      providers: [
+        { provide: DialogService, useValue: dialogServiceMock }
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -19,9 +33,50 @@ describe('DialogComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  it('TODO');
+  it('should display modal with title and message when receive data from observable', () => {
+    spyOn(component.modal, 'show');
+
+    const data: Dialog = {
+      title: 'Title',
+      message: 'Message',
+      success: () => {}
+    };
+
+    dialogServiceMock.data$.next(data);
+
+    expect(component.modal.show).toHaveBeenCalled();
+
+    fixture.detectChanges();
+
+    const title = fixture.nativeElement.querySelector('.modal-title').textContent;
+    const message = fixture.nativeElement.querySelector('.modal-body span').textContent;
+
+    expect(title).toBe(data.title);
+    expect(message).toBe(data.message);
+  });
+
+  it('should call success function when `Yes` button was clicked', fakeAsync(() => {
+    const data: Dialog = {
+      title: 'Title',
+      message: 'Message',
+      success: jasmine.createSpy('success')
+    };
+
+    dialogServiceMock.data$.next(data);
+
+    tick(1000);
+
+    fixture.detectChanges();
+
+    const yesButton = fixture.nativeElement.querySelector('#yes');
+    yesButton.click();
+
+    tick(1000);
+
+    expect(data.success).toHaveBeenCalled();
+  }));
 });
