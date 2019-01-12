1cbe07ce6f799b2444324fc90bbf4007c9a3d059
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-12 17:10:10 -0200

    Criando teste para componente de pagina(login)

--- src/app/auth/pages/login/login.component.spec.ts
+++ src/app/auth/pages/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Store, StoreModule } from '@ngrx/store';
+import * as fromRoot from '../../../reducers';
+import { AppState } from '../../../reducers';
+import { LoginRequested } from '../../auth.actions';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +13,14 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        StoreModule.forRoot({
+          ...fromRoot.reducers
+        })
+      ]
     })
     .compileComponents();
   }));
@@ -19,9 +31,32 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  it('TODO');
+  it('should dispatch LoginRequested action on submit', () => {
+    const store: Store<AppState> = TestBed.get(Store);
+
+    spyOn(store, 'dispatch');
+
+    const data = {
+      email: 'admin@admin.com',
+      password: 'admin'
+    };
+
+    const email: HTMLInputElement = fixture.nativeElement.querySelector('#email');
+    email.value = data.email;
+    email.dispatchEvent(new Event('input'));
+
+    const password: HTMLInputElement = fixture.nativeElement.querySelector('#password');
+    password.value = data.password;
+    password.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    component.onSubmit();
+
+    expect(store.dispatch).toHaveBeenCalledWith(new LoginRequested({ ...data }));
+  });
 });
