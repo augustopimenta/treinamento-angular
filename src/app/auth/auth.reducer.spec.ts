94efbf1089ac6d6253e8f553007daa121511304b
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-12 16:08:40 -0200

    Criando testes para reducers do ngrx

--- src/app/auth/auth.reducer.spec.ts
+++ src/app/auth/auth.reducer.spec.ts
@@ -1,15 +1,71 @@
 import { reducer, initialState } from './auth.reducer';
+import { AuthActionTypes, LoginError, LoginRequested, LoginSucceed, Logout, RegisterFinish, RegisterStarted } from './auth.actions';
+import User from './models/user.model';
 
 describe('Auth reducer', () => {
-  // describe('unknown action', () => {
-  //   it('should return the initial state', () => {
-  //     const action = {} as any;
-  //
-  //     const result = reducer(initialState, action);
-  //
-  //     expect(result).toBe(initialState);
-  //   });
-  // });
+  describe('unknown action', () => {
+    it('should return the initial state', () => {
+      const action = {} as any;
 
-  it('TODO');
+      const result = reducer(undefined, action);
+
+      expect(result).toBe(initialState);
+    });
+  });
+
+  describe('LoginRequested action', () => {
+    it('should set loading to true', () => {
+      const result = reducer(initialState, new LoginRequested({ email: '', password: '' }));
+
+      expect(result).toEqual({ ...initialState, loading: true });
+    });
+  });
+
+  describe('LoginSucceed action', () => {
+    it('should set token, user info and authenticated', () => {
+      const data = {token: 'token', user: { name: 'User' }};
+
+      const result = reducer(initialState, new LoginSucceed(data));
+
+      expect(result).toEqual({ ...initialState, authenticated: true, authToken: data.token, user: data.user });
+    });
+  });
+
+  describe('LoginError action', () => {
+    it('should set authenticated and loading to false', () => {
+      const result = reducer(initialState, new LoginError({ error: '' }));
+
+      expect(result).toEqual({ ...initialState, authenticated: false, loading: false });
+    });
+  });
+
+  describe('RegisterStarted action', () => {
+    it('should set loading to true', () => {
+      const user: User = {
+        name: 'Name',
+        email: 'name@email.com',
+        password: 'password',
+        admin: true
+      };
+      const result = reducer(initialState, new RegisterStarted({ user }));
+
+      expect(result).toEqual( { ...initialState, loading: true });
+    });
+  });
+
+  describe('RegisterFinish action', () => {
+    it('should set loading to false', () => {
+      const result = reducer(initialState, new RegisterFinish());
+
+      expect(result).toEqual( { ...initialState });
+    });
+  });
+
+  describe('Logout action', () => {
+    it('should set state to initialState', () => {
+      const result = reducer(initialState, new Logout({ error: '' }));
+
+      expect(result).toEqual(initialState);
+    });
+  });
 });
--- src/app/dashboard/dashboard.reducer.spec.ts
+++ src/app/dashboard/dashboard.reducer.spec.ts
@@ -1,15 +1,109 @@
 import { reducer, initialState } from './dashboard.reducer';
+import {
+  ChangeSelectedMonth,
+  CreatePurchase,
+  DeletePurchase,
+  FetchPurchases,
+  RequestedPurchases,
+  UpdatePurchase
+} from './dashboard.actions';
+import Purchase from './models/purchase.model';
 
-describe('Dashboard Reducer', () => {
-  // describe('unknown action', () => {
-  //   it('should return the initial state', () => {
-  //     const action = {} as any;
-  //
-  //     const result = reducer(initialState, action);
-  //
-  //     expect(result).toBe(initialState);
-  //   });
-  // });
+describe('Dashboard reducer', () => {
+  describe('unknown action', () => {
+    it('should return the initial state', () => {
+      const action = {} as any;
 
-  it('TODO');
+      const result = reducer(undefined, action);
+
+      expect(result).toBe(initialState);
+    });
+  });
+
+  describe('RequestedPurchases action', () => {
+    it('should set loading to true', () => {
+      const result = reducer(initialState, new RequestedPurchases());
+
+      expect(result).toEqual({ ...initialState, loading: true });
+    });
+  });
+
+  describe('FetchPurchases action', () => {
+    const purchases: Purchase[] = [
+      {id: '1', description: 'd', date: '2019-01-01', total: 10, quantity: 5, value: 2, paid: true },
+      {id: '2', description: 'd', date: '2019-01-02', total: 10, quantity: 5, value: 2, paid: false },
+    ];
+
+    it('should fill purchases', () => {
+      const result = reducer(initialState, new FetchPurchases({ purchases }));
+
+      expect(result).toEqual({ ...initialState, purchases });
+    });
+  });
+
+  describe('CreatePurchase action', () => {
+    const purchase: Purchase = {
+      id: '1',
+      description: 'd',
+      date: '2019-01-01',
+      total: 10,
+      quantity: 5,
+      value: 2,
+      paid: true
+    };
+
+    it('should add new purchase', () => {
+      const result = reducer(initialState, new CreatePurchase({ purchase }));
+
+      expect(result).toEqual({ ...initialState, purchases: [ purchase ] });
+    });
+  });
+
+  describe('UpdatePurchase action', () => {
+    const purchase: Purchase = {
+      id: '1',
+      description: 'd',
+      date: '2019-01-01',
+      total: 10,
+      quantity: 5,
+      value: 2,
+      paid: true
+    };
+
+    it('should update an existent purchase', () => {
+      const action = new UpdatePurchase({ purchase: { ...purchase, description: 'ddd' } });
+
+      const result = reducer({ ...initialState, purchases: [ purchase ] }, action);
+
+      expect(result).toEqual({ ...initialState, purchases: [ { ...purchase, description: 'ddd' } ] });
+    });
+  });
+
+  describe('DeletePurchase action', () => {
+    const purchase: Purchase = {
+      id: '1',
+      description: 'd',
+      date: '2019-01-01',
+      total: 10,
+      quantity: 5,
+      value: 2,
+      paid: true
+    };
+
+    it('should delete an existent purchase', () => {
+      const result = reducer({ ...initialState, purchases: [ purchase ] }, new DeletePurchase({ id: purchase.id }));
+
+      expect(result).toEqual({ ...initialState, purchases: [ ] });
+    });
+  });
+
+  describe('ChangeSelectedMonth action', () => {
+    it('should change selected month index', () => {
+      const index = 2;
+
+      const result = reducer(initialState, new ChangeSelectedMonth({index}));
+
+      expect(result).toEqual({ ...initialState, selectedMonthIndex: index });
+    });
+  });
 });
