ae13996578b8ba046b8d66b85221662633b1848b
Augusto Pimenta <apimenta@luxfacta.com>
2019-01-12 15:56:15 -0200

    Criando testes para selectors do ngrx

--- src/app/auth/auth.selectors.spec.ts
+++ src/app/auth/auth.selectors.spec.ts
@@ -1,3 +1,24 @@
+import * as fromAuth from './auth.selectors';
+import { initialState } from './auth.reducer';
+
 describe('Auth selectors', () => {
-  it('TODO');
+  it('selectAuthState', () => {
+    expect(fromAuth.selectAuthState.projector({ ...initialState })).toEqual(initialState);
+  });
+
+  it('selectAuthAuthenticated', () => {
+    expect(fromAuth.selectAuthAuthenticated.projector({ ...initialState, authenticated: true })).toBe(true);
+  });
+
+  it('selectAuthLoading', () => {
+    expect(fromAuth.selectAuthLoading.projector({ ...initialState, loading: true })).toBe(true);
+  });
+
+  it('selectAuthUser', () => {
+    expect(fromAuth.selectAuthUser.projector({ ...initialState, user: { name: 'name' } })).toEqual({ name: 'name' });
+  });
+
+  it('selectAuthToken', () => {
+    expect(fromAuth.selectAuthToken.projector({ ...initialState, authToken: 'token' })).toBe('token');
+  });
 });
--- src/app/dashboard/dashboard.selectors.spec.ts
+++ src/app/dashboard/dashboard.selectors.spec.ts
@@ -1,3 +1,78 @@
+import * as fromDashboard from './dashboard.selectors';
+import { initialState } from './dashboard.reducer';
+import Purchase from './models/purchase.model';
+
 describe('Dashboard selectors', () => {
-  it('TODO');
+  it('selectDashboardState', () => {
+    expect(fromDashboard.selectDashboardState.projector({ ...initialState })).toEqual(initialState);
+  });
+
+  it('selectDashboardLoading', () => {
+    expect(fromDashboard.selectDashboardLoading.projector({ ...initialState, loading: true })).toBe(true);
+  });
+
+  it('selectDashboardTotals', () => {
+    const purchases: Purchase[] = [
+      {id: '1', description: 'd', date: '2019-01-01', total: 10, quantity: 5, value: 2, paid: true },
+      {id: '2', description: 'd', date: '2019-01-02', total: 10, quantity: 5, value: 2, paid: false },
+    ];
+
+    const expected = {
+      total: 20, pending: 10
+    };
+
+    expect(fromDashboard.selectDashboardTotals.projector({ ...initialState, purchases })).toEqual(expected);
+  });
+
+  it('selectDashboardSelectedMonth', () => {
+    expect(fromDashboard.selectDashboardSelectedMonth.projector({ ...initialState, selectedMonthIndex: 1 })).toBe(1);
+  });
+
+  it('selectDashboardMonthGroups', () => {
+    const purchases: Purchase[] = [
+      {id: '1', description: 'd', date: '2019-01-01', total: 10, quantity: 5, value: 2, paid: true },
+      {id: '2', description: 'd', date: '2019-01-02', total: 10, quantity: 5, value: 2, paid: false },
+    ];
+
+    const expected = [
+      { date: new Date(2019, 0, 1), total: 20, pending: 10 },
+      { date: new Date(2018, 11, 1), total: 0, pending: 0 },
+      { date: new Date(2018, 10, 1), total: 0, pending: 0 },
+      { date: new Date(2018, 9, 1), total: 0, pending: 0 },
+    ];
+
+    const today = new Date(2019, 0, 3);
+
+    expect(fromDashboard.selectDashboardMonthGroups(today).projector( {...initialState, purchases})).toEqual(expected);
+  });
+
+  it('selectDashboardSelectedPurchases', () => {
+    const purchases: Purchase[] = [
+      {id: '3', description: 'd', date: '2018-12-01', total: 10, quantity: 5, value: 2, paid: true },
+      {id: '2', description: 'd', date: '2019-01-02', total: 10, quantity: 5, value: 2, paid: false },
+      {id: '5', description: 'd', date: '2019-01-02', total: 10, quantity: 5, value: 2, paid: false },
+      {id: '1', description: 'd', date: '2019-01-01', total: 10, quantity: 5, value: 2, paid: true },
+      {id: '4', description: 'd', date: '2019-01-03', total: 10, quantity: 5, value: 2, paid: false },
+    ];
+
+    const expected: Purchase[] = [
+      {id: '4', description: 'd', date: '2019-01-03', total: 10, quantity: 5, value: 2, paid: false },
+      {id: '2', description: 'd', date: '2019-01-02', total: 10, quantity: 5, value: 2, paid: false },
+      {id: '5', description: 'd', date: '2019-01-02', total: 10, quantity: 5, value: 2, paid: false },
+      {id: '1', description: 'd', date: '2019-01-01', total: 10, quantity: 5, value: 2, paid: true },
+    ];
+
+    const groups = [
+      { date: new Date(2019, 0, 1), total: 0, pending: 0 },
+      { date: new Date(2018, 11, 1), total: 0, pending: 0 },
+      { date: new Date(2018, 10, 1), total: 0, pending: 0 },
+      { date: new Date(2018, 9, 1), total: 0, pending: 0 },
+    ];
+
+    const today = new Date(2019, 0, 3);
+
+    const result = fromDashboard.selectDashboardSelectedPurchases(today).projector( {...initialState, purchases}, groups);
+
+    expect(result).toEqual(expected);
+  });
 });
